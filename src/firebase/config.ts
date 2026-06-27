92851812b8f22aa6592dcd81f5efb9ca9b0047c8
asare George <asareg365@gmail.com>
2026-06-27 15:30:54 +0000

    yes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: "REPLACEME",
-  authDomain: "REPLACEME",
-  projectId: "REPLACEME",
-  storageBucket: "REPLACEME",
-  messagingSenderId: "REPLACEME",
-  appId: "REPLACEME"
+  apiKey: "AIzaSyAvgUEA_ejb2n3ISKiCxzrtbRmqCsiDoHc",
+  authDomain: "studio-7948451861-17b46.firebaseapp.com",
+  projectId: "studio-7948451861-17b46",
+  storageBucket: "studio-7948451861-17b46.firebasestorage.app",
+  messagingSenderId: "228794187571",
+  appId: "1:228794187571:web:deec76645ef499b57e4846"
 };
